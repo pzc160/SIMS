8311df20f37b21277b5dd578ddd4e3a8592f216e
xiaoyi long <1260269038@qq.com>
2020-05-06 18:01:21 +0800

    完成登陆界面设计，返乡信息界面设计，初步MVC模型搭建，简单增删改查，但有很多BUG待修复

---
+++ Stu.Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stu.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Destination { get; set; }
+        public string Departure_date { get; set; }
+        public string Return_date { get; set; }
+
+        public Student() { }//两种构造方法
+        public Student(int ID,string name,string destination,string departure_date,string return_date)
+        {
+            this.ID = ID;
+            this.Name= name;
+            this.Destination = destination;
+            this.Departure_date = destination;
+            this.Return_date = return_date;
+        }
+       }
+}
